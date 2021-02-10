c64338977f9ea1278c17f582aa0bf822e9f4b686
drop <kntaaa000@gmail.com>
2021-02-11 03:21:03 +0900

    edit template

--- app.py
+++ app.py
@@ -1,29 +1,24 @@
-from flask import Flask, jsonify, abort, make_response,render_template,url_for,request,redirect,send_file
+from flask import Flask, jsonify, abort, make_response, render_template, request
 import os.path
-from imagenet import imagenet
 from PIL import Image
 import io
-import qrcode as qr
 import base64
-from keras.applications.mobilenet import MobileNet, preprocess_input, decode_predictions
-from keras.models import load_model
-import json
+import time
+import urllib.request, json
+import ast
+
 
 app = Flask(__name__)
 
-app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024
-with open('./model/imagenet_class_index.json',encoding="utf-8") as f:
-    zisyo = json.load(f)
-    zisyo = {item["en"]: item["ja"] for item in zisyo}
-model = MobileNet(input_shape=(128,128,3), alpha=1.0, depth_multiplier=1, dropout=1e-3, include_top=True, weights=None, input_tensor=None, pooling=None, classes=1000)
-model.load_weights("./model/kerasmobilenet.h5")
-bunrui = imagenet(model)
-@app.route("/")
+
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/send',methods = ['post'])
+
+@app.route('/send',methods = ['POST'])
 def posttest():
+    from flask import request
     img_file = request.files['img_file']
     fileName = img_file.filename
     root, ext = os.path.splitext(fileName)
@@ -32,19 +27,43 @@ def posttest():
                       ".pxm", ".pnm",  ".sr",  ".ras", ".tiff", ".tif", ".exr", ".hdr", ".pic", ".dib"])
     if ext not in gazouketori:
         return render_template('index.html',massege = "対応してない拡張子です",color = "red")
-    print("success")
-    try:
-        desc, score =  bunrui.predict(img_file)
-        for i in range(5):
-            desc[i] = [i+1,zisyo[desc[i]],round(score[i],1)]
-    except:
-        return render_template('index.html',massege = "解析出来ませんでした",color = "red")
+    # print("success")
+
     buf = io.BytesIO()
     image = Image.open(img_file)
-    image.save(buf, 'png')
     qr_b64str = base64.b64encode(buf.getvalue()).decode("utf-8")
     qr_b64data = "data:image/png;base64,{}".format(qr_b64str)
-    return render_template('kekka.html',namae = desc ,img = qr_b64data)
+
+    url = "https://ms4dcl5u4g.execute-api.us-east-2.amazonaws.com/Prod/post" 
+    method = "POST"
+    headers = {"Content-Type" : "application/json"}
+
+    # PythonオブジェクトをJSONに変換する
+    obj = {"image" : qr_b64data} 
+    json_data = json.dumps(obj).encode("utf-8")
+
+
+    # httpリクエストを準備してPOST
+    rq = urllib.request.Request(url, data=json_data, method=method, headers=headers)
+    with urllib.request.urlopen(rq) as response:
+        print(response)
+        response_body = response.read().decode("utf-8")
+        print(response_body)
+        print(type(response_body))
+
+        # str = "{'名前':'太郎', '身長':'165', '体重':'60'}"
+        response_body = ast.literal_eval(response_body)
+        xy_ratio = response_body["xy_ratio"]
+        bw_ratio = response_body["bw_ratio"]
+        color = response_body["color"]
+        sanpakugan = response_body["sanpakugan"]
+
+    if sanpakugan>1:
+        sapnakugan_result = "あなたは三白眼の傾向があります"
+    else:
+        sapnakugan_result = "あなたは三白眼の傾向がありません"
+
+    return render_template('kekka.html', xy=xy_ratio, bw1=bw_ratio[0], bw2=bw_ratio[1], h=color[0], s=color[1], v=color[2], sanpaku=sapnakugan_result, img = qr_b64data)
 
 @app.route('/kekka')
 def kekka():
@@ -63,5 +82,6 @@ def nosubmit(error):
 @app.errorhandler(503)
 def all_error_handler(error):
      return 'InternalServerError\n', 503
+
 if __name__ == '__main__':
     app.run()
